98b1d79e91347197de2a4db7898bac3bf58ca404
Yordan Dimitrov <y.dimitrov84@gmail.com>
2025-06-23 00:43:17 +0300

    Add files via upload

---
+++ ExtentReportsDemo/src/test/java/Dani/ExtentReportsDemo/ExtentReport.java
@@ -0,0 +1,49 @@
+package Dani.ExtentReportsDemo;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExtentReport {
+	
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config() {
+		
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
+		// create html report
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation Results");
+		reporter.config().setDocumentTitle("Test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Dani");
+		
+	}
+	
+	@Test
+	public void initialDemo() {
+		// create report
+		ExtentTest test = extent.createTest("Initial Demo");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Install\\chromedriver-win64\\chromedriver.exe");		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		System.out.println(driver.getTitle());
+		driver.close();
+		
+		test.fail("Result do not match");
+		
+		// clear report
+		extent.flush();
+	}
+
+}
