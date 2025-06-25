ba6b29667a495c3b29cf2edd8418a7e1f8d4f1cb
Yordan Dimitrov <y.dimitrov84@gmail.com>
2025-06-25 16:46:56 +0300

    Selenium Grid

---
+++ SeleniumGrid/src/GoogleTest.java
@@ -0,0 +1,32 @@
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class GoogleTest {
+	
+	@Test
+	public void HomePageCheck() throws MalformedURLException, URISyntaxException {
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setBrowserName("chrome");
+		//caps.setVersion("138.0.7204.50");
+		//caps.setPlatform(Platform.WIN11);
+		//caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+		
+		WebDriver driver = new RemoteWebDriver(new URI("http://192.168.100.114:4444").toURL(), caps);
+		driver.get("https://github.com/");
+		System.out.println(driver.getTitle());
+		
+		driver.close();
+	}
+
+}
---
+++ SeleniumGrid/src/RSATest.java
@@ -0,0 +1,31 @@
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class RSATest {
+	
+	@Test
+	public void HomePageCheck() throws MalformedURLException, URISyntaxException {
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		//caps.setBrowserName("chrome");
+		//caps.setPlatform(Platform.WIN11);
+		caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+		
+		WebDriver driver = new RemoteWebDriver(new URI("http://192.168.100.114:4444").toURL(), caps);
+		driver.get("http://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		
+		driver.close();
+	}
+
+}
