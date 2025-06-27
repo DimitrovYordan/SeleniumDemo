3bb4f7489abb772c329180aef706b9d30bd9af86
Yordan Dimitrov <y.dimitrov84@gmail.com>
2025-06-27 13:48:22 +0300

    Update WindowActivities.java

--- Introduction/src/WindowActivities.java
+++ Introduction/src/WindowActivities.java
@@ -5,7 +5,7 @@ public class WindowActivities {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Install\\chromedriver-win64\\chromedriver.exe");		
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);		
 		WebDriver chromeDriver = new ChromeDriver();
 		chromeDriver.manage().window().maximize();
 		chromeDriver.get("https://google.com"); // зарежда всички елементи на страницата и тогава продължава към следващата
