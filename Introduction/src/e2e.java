a837169c1da844691cacb05c110e56f6646450f0
Yordan Dimitrov <y.dimitrov84@gmail.com>
2025-06-27 13:48:33 +0300

    Update e2e.java

--- Introduction/src/e2e.java
+++ Introduction/src/e2e.java
@@ -7,7 +7,7 @@ public class e2e {
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Install\\chromedriver-win64\\chromedriver.exe");		
+		System.setProperty("webdriver.chrome.driver", chromeDriverPath);		
 		WebDriver chromeDriver = new ChromeDriver();
 		chromeDriver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 		// flight destination
