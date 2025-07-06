43e78adacc382bb0808208c3091e9ecf64ec49ee
Yordan Dimitrov <y.dimitrov84@gmail.com>
2025-07-06 18:59:03 +0300

    Add files via upload

---
+++ MavenJava/src/test/java/DemoMaven2/MavenJava/AppiumTest.java
@@ -0,0 +1,16 @@
+package DemoMaven2.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void AppiumTest1() {
+		System.out.println("AppiumTest1");
+	}
+	
+	@Test
+	public void AppiumTest2() {
+		System.out.println("AppiumTest2");
+	}
+}
---
+++ MavenJava/src/test/java/DemoMaven2/MavenJava/RESTAPITest.java
@@ -0,0 +1,16 @@
+package DemoMaven2.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void RESTAPITest1() {
+		System.out.println("RESTAPITest1");
+	}
+	
+	@Test
+	public void RESTAPITest2() {
+		System.out.println("RESTAPITest2");
+	}
+}
